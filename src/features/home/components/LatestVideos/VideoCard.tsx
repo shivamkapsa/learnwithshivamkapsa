a52e09f39f53860873b338e6c6d6ac5bb4f0242f
shivamkapsa <shivammishra567612@gmail.com>
2026-08-08 15:52:05 +0530

    Video card

--- src/features/home/components/LatestVideos/VideoCard.tsx
+++ src/features/home/components/LatestVideos/VideoCard.tsx
@@ -28,6 +28,7 @@ export function VideoCard({
           src={thumbnail}
           alt={title}
           fill
+          sizes="(max-width: 768px) 100vw, (max-width: 1024px) 50vw, 33vw"
           className="object-cover"
         />
 
